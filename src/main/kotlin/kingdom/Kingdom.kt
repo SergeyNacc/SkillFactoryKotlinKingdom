f6ea190e5475b8d0ce6df20896161ba76cf1671c
sergey_nacc <sergeynaksin@gmail.com>
2020-11-05 23:15:11 +0700

    added name and characteristics

--- src/main/kotlin/kingdom/Kingdom.kt
+++ src/main/kotlin/kingdom/Kingdom.kt
@@ -2,24 +2,29 @@ package kingdom
 
 fun main() {
 
-}
+    val kingdom = Kingdom()
 
-class Kingdom () {
-
-}
-
-class Ruler() {
+    println("Name: ${kingdom.ruler.name}. Power: ${kingdom.ruler.power}. Intellect: ${kingdom.ruler.intellect}.")
 
 }
 
-class Heir() {
+class Kingdom {
+    val ruler = Ruler("Sergio")
+}
+
+open class Ruler(val name: String) {
+    val power = 100f
+    val intellect = 100f
+}
+
+class Heir(name: String): Ruler(name) {
 
 }
 
-data class Archer() {
+data class Archer (val bow: String, val dagger: String) {
 
 }
 
-data class Warrior() {
-    
+data class Warrior (val weapon: String) {
+
 }
