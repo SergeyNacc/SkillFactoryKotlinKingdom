3d7308fc43b803566cbeb21fcc31edbd58e8db39
sergey_nacc <sergeynaksin@gmail.com>
2020-11-06 14:48:50 +0700

    added weapons to archers and warriors

--- src/main/kotlin/kingdom/Kingdom.kt
+++ src/main/kotlin/kingdom/Kingdom.kt
@@ -6,10 +6,38 @@ fun main() {
 
     println("Name: ${kingdom.ruler.name}. Power: ${kingdom.ruler.power}. Intellect: ${kingdom.ruler.intellect}.")
 
+
+    println(kingdom.archers)
+    println(kingdom.warriors)
+
 }
 
 class Kingdom {
     val ruler = Ruler("Sergio")
+    val heirs = mutableListOf<Heir>()
+
+    val archers = mutableListOf<Archer>()
+    val warriors = mutableListOf<Warrior>()
+
+    init {
+        for (i in 0..19) {
+            if (i % 2 == 0) {
+                archers.add(Archer("Dagger"))
+            } else {
+                archers.add(Archer("None"))
+            }
+        }
+
+        for (i in 0..29) {
+            if (i % 2 == 0) {
+                warriors.add(Warrior("Sword"))
+            } else {
+                warriors.add(Warrior("Axe"))
+            }
+        }
+    }
+
+
 }
 
 open class Ruler(val name: String) {
@@ -21,7 +49,7 @@ class Heir(name: String): Ruler(name) {
 
 }
 
-data class Archer (val bow: String, val dagger: String) {
+data class Archer(val bow: String) {
 
 }
 
